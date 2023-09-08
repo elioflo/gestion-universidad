a25e12363773a6c73b46600a1d19fe1e9659e40a
Elio Flores <eliofloreschoque2357@gmail.com>
2023-09-08 18:36:23 -0300

    Creacion de proyecto

---
+++ src/entidades/Materia.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author valef
+ */
+public class Materia {
+    private int idMateria, anioMateria;
+    private String nombre;
+    private boolean estado;
+
+    public Materia() {
+    }
+
+    public Materia(int idMateria, int anioMateria, String nombre, boolean estado) {
+        this.idMateria = idMateria;
+        this.anioMateria = anioMateria;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public Materia(int anioMateria, String nombre, boolean estado) {
+        this.anioMateria = anioMateria;
+        this.nombre = nombre;
+        this.estado = estado;
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public int getAnioMateria() {
+        return anioMateria;
+    }
+
+    public void setAnioMateria(int anioMateria) {
+        this.anioMateria = anioMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", anioMateria=" + anioMateria + ", nombre=" + nombre + ", estado=" + estado + '}';
+    }
+
+    
+    
+    
+}
