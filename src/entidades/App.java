6a481f710124106d93064a41029951d39fa160fb
Elio Flores <eliofloreschoque2357@gmail.com>
2023-09-11 20:48:31 -0300

    Completo metodos de inscripcionDAta

--- src/entidades/App.java
+++ src/entidades/App.java
@@ -1,15 +1,18 @@
 package entidades;
 
-import accesoDeDatos.AlumnoData;
-import java.time.LocalDate;
+import accesoDeDatos.InscripcionData;
+import java.util.List;
 
 public class App {
 
   public static void main(String[] args) {
-    Alumno juan = new Alumno(34568653, "Luna", "Pedro", LocalDate.of(1980, 4, 25), true);
-    AlumnoData alu = new AlumnoData();
-    alu.guardarAlumno(juan);
+//    Alumno juan = new Alumno(34568653, "Luna", "Pedro", LocalDate.of(1980, 4, 25), true);
+//    AlumnoData alu = new AlumnoData();
+//    alu.guardarAlumno(juan);
 
+    InscripcionData inscripcionData = new InscripcionData();
+    List<Inscripcion> inscripciones = inscripcionData.obtenerInscripciones();
+    inscripciones.forEach(inscripcion -> System.out.println(inscripcion.getIdInscripcion() + ":" + inscripcion.getNota()));
   }
-
+  
 }
