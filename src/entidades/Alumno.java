8847dbcee60e37c5ac7a9d5fddbf7d07e685ecf6
Elio Flores <eliofloreschoque2357@gmail.com>
2023-09-13 18:03:27 -0300

    Se listan alumnos en formulario de inscripcion.

--- src/entidades/Alumno.java
+++ src/entidades/Alumno.java
@@ -12,85 +12,84 @@ import java.time.LocalDate;
  * @author valef
  */
 public class Alumno {
+
   private int idAlumno, dni;
   private String apellido, nombre;
   private LocalDate fechaNacimiento;
- private boolean estado;
+  private boolean estado;
 
-    public Alumno() {
-    }
+  public Alumno() {
+  }
 
-    public Alumno(int dni, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) {
-        this.dni = dni;
-        this.apellido = apellido;
-        this.nombre = nombre;
-        this.fechaNacimiento = fechaNacimiento;
-        this.estado = estado;
-    }
-    
-    public Alumno(int idAlumno, int dni, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) {
-        this.idAlumno = idAlumno;
-        this.dni = dni;
-        this.apellido = apellido;
-        this.nombre = nombre;
-        this.fechaNacimiento = fechaNacimiento;
-        this.estado = estado;
-    }
+  public Alumno(int dni, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) {
+    this.dni = dni;
+    this.apellido = apellido;
+    this.nombre = nombre;
+    this.fechaNacimiento = fechaNacimiento;
+    this.estado = estado;
+  }
 
-    public int getIdAlumno() {
-        return idAlumno;
-    }
+  public Alumno(int idAlumno, int dni, String apellido, String nombre, LocalDate fechaNacimiento, boolean estado) {
+    this.idAlumno = idAlumno;
+    this.dni = dni;
+    this.apellido = apellido;
+    this.nombre = nombre;
+    this.fechaNacimiento = fechaNacimiento;
+    this.estado = estado;
+  }
 
-    public void setIdAlumno(int idAlumno) {
-        this.idAlumno = idAlumno;
-    }
+  public int getIdAlumno() {
+    return idAlumno;
+  }
 
-    public String getApellido() {
-        return apellido;
-    }
+  public void setIdAlumno(int idAlumno) {
+    this.idAlumno = idAlumno;
+  }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+  public String getApellido() {
+    return apellido;
+  }
 
-    public String getNombre() {
-        return nombre;
-    }
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+  public String getNombre() {
+    return nombre;
+  }
 
-    public LocalDate getFechaNacimiento() {
-        return fechaNacimiento;
-    }
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 
-    public void setFechaNacimiento(LocalDate fechaNacimiento) {
-        this.fechaNacimiento = fechaNacimiento;
-    }
+  public LocalDate getFechaNacimiento() {
+    return fechaNacimiento;
+  }
 
-    public boolean getEstado() {
-        return estado;
-    }
+  public void setFechaNacimiento(LocalDate fechaNacimiento) {
+    this.fechaNacimiento = fechaNacimiento;
+  }
 
-    public void setEstado(boolean estado) {
-        this.estado = estado;
-    }
-//faltaba el atributo dni 
-    
-    public int getDni() {
-        return dni;
-    }
+  public boolean getEstado() {
+    return estado;
+  }
 
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
+  public void setEstado(boolean estado) {
+    this.estado = estado;
+  }
+//faltaba el atributo dni
 
-    @Override
-    public String toString() {
-        return "Alumno{" + "idAlumno=" + idAlumno + ", apellido=" + apellido + ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", estado=" + estado + '}';
-    }
-   
- 
+  public int getDni() {
+    return dni;
+  }
+
+  public void setDni(int dni) {
+    this.dni = dni;
+  }
+
+  @Override
+  public String toString() {
+    return dni + ", " + apellido + ", " + nombre;
+  }
+
 }
- 
