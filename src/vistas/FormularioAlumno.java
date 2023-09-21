2307ac3324019acb95201745101c505db3e4f8e9
LucaDap2805 <lucapianojunior@gmail.com>
2023-09-21 03:13:36 -0300

    cambios puramente esteticos

--- src/vistas/FormularioAlumno.java
+++ src/vistas/FormularioAlumno.java
@@ -7,6 +7,7 @@ package vistas;
 
 import accesoDeDatos.AlumnoData;
 import entidades.Alumno;
+import java.awt.event.ActionEvent;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -66,6 +67,12 @@ public class FormularioAlumno extends javax.swing.JInternalFrame {
         jLabel1.setText("Información de los alumnos");
 
         jLabel2.setText("Documento:");
+
+        documento_text.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                documento_textActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("Apellido:");
 
@@ -198,8 +205,8 @@ public class FormularioAlumno extends javax.swing.JInternalFrame {
                     .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 27, Short.MAX_VALUE)
-                    .addComponent(fecha_chooser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(fecha_chooser, javax.swing.GroupLayout.DEFAULT_SIZE, 20, Short.MAX_VALUE))
                 .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(nuevoAlumno_boton)
@@ -221,7 +228,7 @@ public class FormularioAlumno extends javax.swing.JInternalFrame {
         if(chequeoDocumento()){
             if (data.buscarAlumnoPorDni(Integer.parseInt(documento_text.getText()))!=null){
             try {
-                Long lo = Long.parseLong(documento_text.getText());
+                Integer lo = Integer.parseInt(documento_text.getText());
                 
                 Alumno alumno = data.buscarAlumnoPorDni(Integer.parseInt(documento_text.getText()));
                 apellido_text.setText(alumno.getApellido());
@@ -289,6 +296,10 @@ public class FormularioAlumno extends javax.swing.JInternalFrame {
         estado_boton.setSelected(false);
         fecha_chooser.setDate(null);
     }//GEN-LAST:event_limpiar_botonActionPerformed
+
+    private void documento_textActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_documento_textActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_documento_textActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
